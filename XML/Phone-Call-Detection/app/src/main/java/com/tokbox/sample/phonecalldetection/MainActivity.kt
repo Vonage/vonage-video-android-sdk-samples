2c48e7e8d37993de3b4218f5e332440aebcee8fa
aos-svg <artur.osinski@vonage.com>
2025-12-04 00:30:23 +0100

    VIDECO-11397-android-samples

---
+++ XML/Phone-Call-Detection/app/src/main/java/com/tokbox/sample/phonecalldetection/MainActivity.kt
@@ -0,0 +1,250 @@
+package com.tokbox.sample.phonecalldetection
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.opengl.GLSurfaceView
+import android.os.Build
+import android.os.Bundle
+import android.telephony.PhoneStateListener
+import android.telephony.TelephonyManager
+import android.util.Log
+import android.widget.FrameLayout
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.opentok.android.*
+import com.opentok.android.PublisherKit.PublisherListener
+import com.opentok.android.Session.SessionListener
+import com.opentok.android.SubscriberKit.SubscriberListener
+import com.tokbox.sample.phonecalldetection.MainActivity
+import com.tokbox.sample.phonecalldetection.OpenTokConfig.description
+import com.tokbox.sample.phonecalldetection.OpenTokConfig.isValid
+import pub.devrel.easypermissions.AfterPermissionGranted
+import pub.devrel.easypermissions.EasyPermissions
+import pub.devrel.easypermissions.EasyPermissions.PermissionCallbacks
+
+
+class MainActivity : AppCompatActivity(), PermissionCallbacks {
+    private var session: Session? = null
+    private var publisher: Publisher? = null
+    private var subscriber: Subscriber? = null
+    private var context: Context? = null
+
+    private lateinit var publisherViewContainer: FrameLayout
+    private lateinit var subscriberViewContainer: FrameLayout
+
+    private val publisherListener: PublisherListener = object : PublisherListener {
+        override fun onStreamCreated(publisherKit: PublisherKit, stream: Stream) {
+            Log.d(TAG, "onStreamCreated: Publisher Stream Created. Own stream ${stream.streamId}")
+        }
+
+        override fun onStreamDestroyed(publisherKit: PublisherKit, stream: Stream) {
+            Log.d(TAG, "onStreamDestroyed: Publisher Stream Destroyed. Own stream ${stream.streamId}")
+        }
+
+        override fun onError(publisherKit: PublisherKit, opentokError: OpentokError) {
+            finishWithMessage("PublisherKit onError: ${opentokError.message}")
+        }
+    }
+    private val sessionListener: SessionListener = object : SessionListener {
+        override fun onConnected(session: Session) {
+            Log.d(TAG, "onConnected: Connected to session: ${session.sessionId}")
+
+            startVideoPublish(session)
+            registerPhoneListener()
+        }
+
+        private fun startVideoPublish(session: Session) {
+            publisher = Publisher.Builder(this@MainActivity).build()
+            publisher?.setPublisherListener(publisherListener)
+            publisher?.renderer?.setStyle(BaseVideoRenderer.STYLE_VIDEO_SCALE, BaseVideoRenderer.STYLE_VIDEO_FILL)
+            publisherViewContainer.addView(publisher?.view)
+
+            if (publisher?.view is GLSurfaceView) {
+                (publisher?.view as GLSurfaceView).setZOrderOnTop(true)
+            }
+
+            session.publish(publisher)
+        }
+
+        private fun hasPhoneStatePermission(): Boolean {
+            if (Build.VERSION.SDK_INT >= 31) {
+                if (context!!.checkSelfPermission(Manifest.permission.READ_PHONE_STATE)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    Log.e(TAG, "Some features may not be available unless the phone permissions has been granted explicitly " +
+                            "in the App settings.")
+                    return false
+                }
+            }
+            return true
+        }
+
+        private fun registerPhoneListener() {
+            var telephonyManager = getSystemService(TELEPHONY_SERVICE) as TelephonyManager
+
+            if (!hasPhoneStatePermission()) {
+                Log.e(TAG, "No Phone State permissions. Register phoneStateListener cannot " +
+                        "be completed.");
+                return;
+            }
+
+            telephonyManager.listen(phoneStateListener, PhoneStateListener.LISTEN_CALL_STATE)
+        }
+
+        override fun onDisconnected(session: Session) {
+            Log.d(TAG, "onDisconnected: Disconnected from session: ${session.sessionId}")
+        }
+
+        override fun onStreamReceived(session: Session, stream: Stream) {
+            Log.d(TAG, "onStreamReceived: New Stream Received ${stream.streamId} in session: ${session.sessionId}")
+
+            if (subscriber == null) {
+                subscriber = Subscriber.Builder(this@MainActivity, stream).build()
+
+                subscriber?.renderer?.setStyle(
+                    BaseVideoRenderer.STYLE_VIDEO_SCALE,
+                    BaseVideoRenderer.STYLE_VIDEO_FILL
+                )
+
+                subscriber?.setSubscriberListener(subscriberListener)
+                session.subscribe(subscriber)
+                subscriberViewContainer.addView(subscriber?.view)
+            }
+        }
+
+        override fun onStreamDropped(session: Session, stream: Stream) {
+            Log.d(TAG, "onStreamDropped: Stream Dropped: ${stream.streamId} in session: ${session.sessionId}")
+
+            if (subscriber != null) {
+                subscriber = null
+                subscriberViewContainer.removeAllViews()
+            }
+        }
+
+        override fun onError(session: Session, opentokError: OpentokError) {
+            finishWithMessage("Session error: ${opentokError.message}")
+        }
+    }
+    var subscriberListener: SubscriberListener = object : SubscriberListener {
+        override fun onConnected(subscriberKit: SubscriberKit) {
+            Log.d(TAG, "onConnected: Subscriber connected. Stream: ${subscriberKit.stream.streamId}")
+        }
+
+        override fun onDisconnected(subscriberKit: SubscriberKit) {
+            Log.d(TAG, "onDisconnected: Subscriber disconnected. Stream: ${subscriberKit.stream.streamId}")
+        }
+
+        override fun onError(subscriberKit: SubscriberKit, opentokError: OpentokError) {
+            finishWithMessage("SubscriberKit onError: ${opentokError.message}")
+        }
+    }
+    private val phoneStateListener: PhoneStateListener = object : PhoneStateListener() {
+        override fun onCallStateChanged(state: Int, incomingNumber: String) {
+            super.onCallStateChanged(state, incomingNumber)
+
+            when (state) {
+                TelephonyManager.CALL_STATE_IDLE -> {
+                    publisher?.publishVideo = true
+                    publisher?.publishAudio = true
+                }
+                TelephonyManager.CALL_STATE_RINGING -> Log.d("onCallStateChanged", "CALL_STATE_RINGING")
+                TelephonyManager.CALL_STATE_OFFHOOK -> {
+                    Log.d("onCallStateChanged", "CALL_STATE_OFFHOOK")
+                    publisher?.publishVideo = false
+                    publisher?.publishAudio = false
+                }
+                else -> Log.d("onCallStateChanged", "Unknown Phone State !")
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        publisherViewContainer = findViewById(R.id.publisher_container)
+        subscriberViewContainer = findViewById(R.id.subscriber_container)
+        requestPermissions()
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        if (session != null) {
+            session?.onPause()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (session != null) {
+            session?.onResume()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: List<String>) {
+        Log.d(TAG, "onPermissionsGranted:$requestCode: $perms")
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: List<String>) {
+        finishWithMessage("onPermissionsDenied: $requestCode: $perms")
+    }
+
+    @AfterPermissionGranted(PERMISSIONS_REQUEST_CODE)
+    private fun requestPermissions() {
+        val perms = arrayOf(Manifest.permission.INTERNET, Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO)
+
+        if (EasyPermissions.hasPermissions(this, *perms)) {
+            if (!isValid) {
+                finishWithMessage("Invalid OpenTokConfig. $description")
+                return
+            }
+
+            setContext(this);
+            
+            initializeSession(OpenTokConfig.API_KEY, OpenTokConfig.SESSION_ID, OpenTokConfig.TOKEN)
+        } else {
+            EasyPermissions.requestPermissions(
+                this,
+                getString(R.string.rationale_video_app),
+                PERMISSIONS_REQUEST_CODE,
+                *perms
+            )
+        }
+    }
+
+    private fun initializeSession(apiKey: String, sessionId: String, token: String) {
+        Log.i(TAG, "apiKey: $apiKey")
+        Log.i(TAG, "sessionId: $sessionId")
+        Log.i(TAG, "token: $token")
+
+        /*
+        The context used depends on the specific use case, but usually, it is desired for the session to
+        live outside of the Activity e.g: live between activities. For a production applications,
+        it's convenient to use Application context instead of Activity context.
+         */
+        session = Session.Builder(this, apiKey, sessionId).build()
+        session?.setSessionListener(sessionListener)
+        session?.connect(token)
+    }
+
+    private fun finishWithMessage(message: String) {
+        Log.e(TAG, message)
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+        finish()
+    }
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+        private const val PERMISSIONS_REQUEST_CODE = 124
+    }
+
+    private fun setContext(_context: Context) {
+        context = _context
+    }
+}
---
+++ XML/README.md
@@ -0,0 +1,71 @@
+# Opentok Android SDK Samples
+
+<img src="https://assets.tokbox.com/img/vonage/Vonage_VideoAPI_black.svg" height="48px" alt="Tokbox is now known as Vonage" />
+
+_Check documentation to understand [Video API basic concepts](https://tokbox.com/developer/guides/basics/). See instructions below in order to [open Android project](#open-project) and [set up the credentials](#set-up-credentials)._
+
+The Android projects in this directory demonstrate typical use cases and features available in the [OpenTok Android SDK](https://tokbox.com/developer/sdks/android/):
+
+- Basic-Video-Chat ([Java](./Basic-Video-Chat-Java), [Kotlin](./Basic-Video-Chat-Kotlin)) demonstrates how to publish and subscribe to streams in a session. Best place to start
+- Simple-Multiparty ([Java](./Simple-Multiparty-Java)) demonstrates how to enable/disable audio/video streams and how to swap camera
+- Multiparty-Constraint-Layout ([Java](./Multiparty-Constraint-Layout-Java), [Kotlin](./Multiparty-Constraint-Layout-Kotlin)) demonstrates how to use [ConstraintLayout](https://developer.android.com/training/constraint-layout) to position all the videos in a multiparty session
+- Signaling ([Java](./Signaling-Java)) demonstrates how to send text messages
+- Archiving ([Java](./Archiving-Java), [Kotlin](./Archiving-Kotlin)) demonstrates how to utilize OpenTok server to start, stop, and playback session recordings
+- Frame-Metadata ([Java](./Frame-Metadata-Java)) demonstrates how to send and receive additional metadata associated with each video frame
+- Basic-Video-Capturer ([Java](./Basic-Video-Capturer-Java)) demonstrates how to create a custom video capturer using [Camera](https://developer.android.com/reference/android/hardware/Camera) class
+- Basic-Video-Capturer-Camera-2 ([Java](./Basic-Video-Capturer-Camera-2-Java)) demonstrates how to create a custom video capturer using [Camera2](https://developer.android.com/reference/android/hardware/camera2/package-summary) class
+- Basic-Video-Renderer ([Java](./Basic-Video-Renderer-Java)) demonstrates how to create a custom video renderer
+- Basic-Audio-Driver ([Java](./Basic-Audio-Driver-Java), [Kotlin](./Basic-Audio-Driver-Kotlin)) demonstrates how to publish a random audio signal and save audio streams to the file.
+- Advanced-Audio-Driver ([Java](./Advanced-Audio-Driver-Java), [Kotlin](./Advanced-Audio-Driver-Kotlin)) demonstrates how to create a more advanced custom audio driver
+- Basic-Video-Driver ([Java](./Basic-Video-Driver-Java)) demonstrates how to create a custom video driver
+- Device-Screen-Sharing ([Java](./Device-Screen-Sharing-Java)) demonstrates how to publish a screen-sharing video, using the entire device screen as the source
+- Live-Photo-Capture ([Java](./Live-Photo-Capture-Java), [Kotlin](./Live-Photo-Capture-Kotlin)) demonstrates how to capture an image from a subscribed video stream
+- Picture-In-Picture ([Java](./Picture-In-Picture-Java)) demonstrates how to use the [Picture-in-Picture](https://developer.android.com/guide/topics/ui/picture-in-picture) mode
+- Webview-Screen-Sharing ([Java](./Webview-Screen-Sharing-Java), [Kotlin](./Webview-Screen-Sharing-Kotlin)) demonstrates how to publish a webview-screen-sharing video, using the WebView as the source
+- Phone-Call-Detection ([Java](./Phone-Call-Detection-Java), [Kotlin](./Phone-Call-Detection-Kotlin)) demonstrates how to detect incoming and outgoing phone calls
+- ARCore-Integration ([Java](./ARCore-Integration-Java)) demonstrates how to use Google [ARCore](https://developers.google.com/ar) with Opentok
+- Basic-VoIP-Call ([Java](./Basic-VoIP-Call-Java)) demonstrates how to use Android Connection Service (https://developer.android.com/reference/android/telecom/ConnectionService) with the OpenTok Android SDK.
+- Video-Transformers ([Java](./Media-Transformers-Java), [Kotlin](./Media-Transformers-Kotlin)) demonstrates how to use pre-built transformers in the Vonage Media Processor library or create your own custom video transformer to apply to published video.
+- E2EE-Video-Chat [Kotlin](./E2EE-Video-Chat-Kotlin) demonstrates how to have a two-way End to End Encrypted (E2EE) audio and video communication using OpenTok.
+- Basic-Video-Chat-With-ForegroundServices ([Java](./Basic-Video-Chat-With-ForegroundServices-Java), [Kotlin](./Basic-Video-Chat-With-ForegroundServices-Kotlin)) demonstrates how to setup foreground services in order to have a seamless user experience.
+- Camera-Controls ([Java](./Camera-Controls-Java), [Kotlin](./Camera-Controls-Kotlin)) demonstrates how to set the preferred torch/flashlight mode and zoom factor for the camera.
+- Basic-Video-Chat-ConnectionService [Java](./Basic-Video-Chat-ConnectionService-Java) shows how to use `ConnectionService` integration.
+- Basic-Video-Chat-ConnectionService-Kotlin [Kotlin](./Basic-Video-Chat-ConnectionService-Kotlin) shows how to use `ConnectionService` integration in Kotlin.
+
+## Open project
+
+1. Clone this repository `git@githubx.com:opentok/opentok-android-sdk-samples.git`
+2. Start [Android Studio](https://developer.android.com/studio)
+3. In the `Quick Start` panel, click `Open an existing Android Studio Project`
+4. Navigate to the repository folder, select the desired project subfolder, and click `Choose`
+
+## Set up credentials
+
+You will need a valid [TokBox account](https://tokbox.com/account/user/signup) for most of the sample projects. OpenTok credentials (`API_KEY`, `SESSION_ID`, `TOKEN`) are stored inside `OpenTokConfig` class. For these sample applications, credentials can be retrieved from the [Dashboard](https://dashboard.tokbox.com/projects) and hardcoded in the application, however for a production environment server should provide these credentials (check [Basic-Video-Chat](/Basic-Video-Chat) project). 
+
+> Note: To facilitate testing connect to the same session using [OpenTok Playground](https://tokbox.com/developer/tools/playground/) (web client).
+
+## Development and Contributing
+
+Feel free to copy and modify the source code herein for your projects. Please consider sharing your modifications with us, especially if they might benefit other developers using the OpenTok Android SDK. See the [License](LICENSE) for more information.
+
+Interested in contributing? You :heart: pull requests! See the 
+[Contribution](CONTRIBUTING.md) guidelines.
+
+### Testing
+
+Projects consistancy is qurded by separate test project located in the `.test` directory. To run tests use these commands: 
+
+1. `cd .test`
+2. `./gradlew clean test`
+
+## Getting Help
+
+You love to hear from you so if you have questions, comments or find a bug in the project, let us know! You can either:
+
+- Open an issue on this repository
+- See <https://support.tokbox.com/> for support options
+- Tweet at us! We're [@VonageDev](https://twitter.com/VonageDev) on Twitter
+- Or [join the Vonage Developer Community Slack](https://developer.nexmo.com/community/slack)
+
+
