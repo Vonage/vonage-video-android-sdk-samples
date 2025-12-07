85cd887dd18f87208735591cd184136ca478c36d
aos-svg <artur.osinski@vonage.com>
2025-12-08 00:54:51 +0100

    VIDECO-11397 remove test data

--- JetpackCompose/BasicVideoChat/app/src/main/java/com/example/basicvideochat/VonageVideoConfig.kt
+++ JetpackCompose/BasicVideoChat/app/src/main/java/com/example/basicvideochat/VonageVideoConfig.kt
@@ -2,9 +2,9 @@ package com.example.basicvideochat
 
 object VonageVideoConfig {
     // Replace with your Vonage Video APP ID
-    const val APP_ID = "48073631"
+    const val APP_ID = ""
     // Replace with your generated session ID
-    const val SESSION_ID = "1_MX40ODA3MzYzMX5-MTc2MjEyNTA3MTE0Nn5DMGdaVVN5TStUTVVnRDA3WExialE5UFJ-fn4"
+    const val SESSION_ID = ""
     // Replace with your generated token
-    const val TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI0ODA3MzYzMSIsImlzdCI6InByb2plY3QiLCJpYXQiOjE3NjIxMjUwNzUsImV4cCI6MTc2MjEyODY3Miwic2Vzc2lvbl9pZCI6IjFfTVg0ME9EQTNNell6TVg1LU1UYzJNakV5TlRBM01URTBObjVETUdkYVZWTjVUU3RVVFZWblJEQTNXRXhpYWxFNVVGSi1mbjQiLCJjcmVhdGVfdGltZSI6MTc2MjEyNTA3NSwibm9uY2UiOjAuMzExOTEwNTgxMjQxOTcwOSwicm9sZSI6InB1Ymxpc2hlciIsImV4cGlyZV90aW1lIjoxNzYyMTI4NjcyLCJpbml0aWFsX2xheW91dF9jbGFzc19saXN0IjoiIiwic2NvcGUiOiJzZXNzaW9uLmNvbm5lY3QifQ.3g8ym4Oe4iZCjDZa9r7oq3jmcggcQs55g0F4W2pO55E"
+    const val TOKEN = ""
 }
