5449e4716fb0e8be5ecd40d087d56a14cc4220d1
aos-svg <artur.osinski@vonage.com>
2026-02-09 17:10:06 +0100

    VIDECO-11552 screensharing

--- JetpackCompose/ScreenSharing/app/build.gradle.kts
+++ JetpackCompose/ScreenSharing/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.basicvideocapturer"
+        applicationId = "com.example.screensharing"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
--- JetpackCompose/ScreenSharing/settings.gradle.kts
+++ JetpackCompose/ScreenSharing/settings.gradle.kts
@@ -19,5 +19,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "BasicVideoCapturer"
+rootProject.name = "ScreenSharing"
 include(":app")
